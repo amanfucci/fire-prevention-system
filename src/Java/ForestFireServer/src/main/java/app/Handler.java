9c4b97d128e4eab738314636dbb5582a65e5079c
Alessandro Manfucci <alessandro.manfucci@polomanettiporciatti.edu.it>
2021-04-05 17:18:08 +0200

    save to DB working

--- src/Java/ForestFireServer/src/main/java/app/Handler.java
+++ src/Java/ForestFireServer/src/main/java/app/Handler.java
@@ -4,6 +4,12 @@ import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import java.nio.ByteOrder;
+import java.text.SimpleDateFormat;  
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.io.*;
 
 public class Handler implements Runnable {
@@ -14,24 +20,76 @@ public class Handler implements Runnable {
         socket = s;
     }
 
+    public static String byteToHex(byte[] arr, int start, int len) {
+        String re = "0x";
+
+        for (int i = start; i < (start + len); i++)
+            re += String.format("%02X", arr[i]);
+
+        return re.replace(" ", "");
+    }
+
+    public static void printByte(byte[] arr, int start, int len) {
+        String re = "";
+        for (int i = start; i < (start + len); i++)
+            re += String.format("%02X ", arr[i]);
+        System.out.println(re);
+    }
+
+    public static int byteToInt(byte[] arr, int start, int len) {
+        int re = 0;
+        Boolean be = ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN);
+
+        for (int i = 0; i < len; i++) {
+            if (be){
+                //re &= (0x0 << (8 * i));
+                re |= ((arr[start + i] & 0xFF) << (8 * i));
+            }
+            else{
+                //re &= (0x0 << (8 * (len - 1 - i)));
+                re |= ((arr[start + i] & 0xFF) << (8 * (len - 1 - i)));
+            }
+        }
+
+        return re;
+    }
+
     @Override
     public void run() {
         try {
             InputStream is = new BufferedInputStream(socket.getInputStream());
             byte[] data = new byte[240];
             int len = is.read(data);
-
-            
-            for (int i = 0; i < len ; i++) {
-                if (i % 20 == 0)
-                    System.out.println();
-                System.out.print(String.format("%02X ", data[i]));
-            }
+            int n_mes = len / 20;
             is.close();
-            // Write to DB
-        } catch (IOException e) {
+
+            // Print to screen
+            for (int i = 0; i < len; i += 20) {
+               printByte(data, i, 20);
+            }
+
+            dataBase db = new dataBase("localhost", "3306", "manfu_lora", "root", "");
+            String sql = "insert into data"
+                    + "(arduinoId, temperature, humidity, CO2, tvoc, updateId, timestamp) values";
+
+            // Save to db
+            for (int i = 0; i < len; i += 20) {
+                if(i!=0)
+                    sql+=",";
+                sql += "(" + byteToHex(data, i, 8) + ",";
+                sql += (int) data[i + 8] + "," + (int) data[i + 9]+",";
+                sql += byteToInt(data, i+10, 2) + "," + byteToInt(data, i+12,2) + ",";
+                sql += byteToInt(data, i+14, 2) + ",";
+                sql += "'"+new Timestamp(new Date().getTime())+"'";
+                sql += ")";
+            }
+            System.out.println(sql);
+            db.manipulate(sql);
+
+        } catch (Exception e) {
             System.out.println(e);
         }
 
     }
+
 }
---
+++ src/Java/ForestFireServer/src/main/java/app/dataBase.java
@@ -0,0 +1,38 @@
+package app;
+
+import java.sql.*;
+import java.sql.DriverManager;
+
+public class dataBase {
+
+    Connection con;
+
+    public dataBase(String host, String port, String db, String user, String pw) throws Exception {
+
+        //Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db,
+         user, pw);
+
+    }
+
+    public void closCon() throws SQLException {
+        con.close();
+    }
+
+    public void query(String sql) throws SQLException{
+        Statement stm = con.createStatement();
+        ResultSet rs = stm.executeQuery(sql);
+        while(rs.next()){
+            
+        }
+    }
+
+    public void manipulate(String sql) throws SQLException{
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.execute();
+    }
+
+    public static void main(String[] args) throws Exception{
+
+    }
+}
